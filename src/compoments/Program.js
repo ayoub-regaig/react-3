76d77935cd32f61c0cc5e025207a9b969d54edb5
ayoub-regaig <44925338+ayoub-regaig@users.noreply.github.com>
2019-06-08 17:47:26 +0200

    react-4

--- src/compoments/Program.js
+++ src/compoments/Program.js
@@ -6,13 +6,18 @@ class Program extends Component {
     }
     render() {
         return (
-            <div className= "program">
+            <div style={pr}>
                 <h1>{this.props.abc}</h1>
                 <p>{this.props.des}</p>
                 <center><button>en savoir plus</button></center>
-
             </div>
-        )
+
+        );
     }
 }
-export default Program
+const pr = {
+    backgroundColor: 'blue',
+    display : 'inline-block',
+    margin: 15
+}
+export default Program;
